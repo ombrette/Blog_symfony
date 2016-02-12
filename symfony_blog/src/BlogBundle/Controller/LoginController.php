855b1945341ff1502a7662640859f2ecedf150b7
LAMBERT Héma <lamberthema@hotmail.fr>
2016-02-12 01:14:44 +0100

    add admin login

---
+++ symfony_blog/src/BlogBundle/Controller/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class LoginController extends Controller
+{
+    public function loginAction(Request $request)
+    {
+    	$authenticationUtils = $this->get('security.authentication_utils');
+
+	    // get the login error if there is one
+	    $error = $authenticationUtils->getLastAuthenticationError();
+
+	    // last username entered by the user
+	    $lastUsername = $authenticationUtils->getLastUsername();
+
+	    return $this->render(
+	        'BlogBundle:Login:login.html.twig',
+	        array(
+	            // last username entered by the user
+	            'last_username' => $lastUsername,
+	            'error'         => $error,
+	        )
+	    );
+    }
+
+    public function loginCheckAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+    }
+}
--- symfony_blog/src/BlogBundle/Entity/User.php
+++ symfony_blog/src/BlogBundle/Entity/User.php
@@ -31,67 +31,68 @@ class User
     /**
      * @var string
      *
-     * @ORM\Column(name="password", type="string", length=255)
+     * @ORM\Column(name="password", type="string", length=64)
      */
     private $password;
 
-
     /**
-     * Get id
-     *
-     * @return int
+     * @ORM\Column(type="string", length=60, unique=true)
      */
+    private $email;
+
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * Set username
-     *
-     * @param string $username
-     *
-     * @return User
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    /**
-     * Get username
-     *
-     * @return string
-     */
     public function getUsername()
     {
         return $this->username;
     }
 
-    /**
-     * Set password
-     *
-     * @param string $password
-     *
-     * @return User
-     */
-    public function setPassword($password)
+    public function getSalt()
     {
-        $this->password = $password;
-
-        return $this;
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
     }
 
-    /**
-     * Get password
-     *
-     * @return string
-     */
     public function getPassword()
     {
         return $this->password;
+    }
+
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt
+        ) = unserialize($serialized);
     }
 }
 
